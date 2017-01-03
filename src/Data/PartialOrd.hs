839f82c9eaa0b0f921ced465f199f3f3f8d3beed
Jason Carr <jac3559@rit.edu>
2017-01-03 14:25:40 -0500

    Updated to base 4.9 with overlapping pragma

--- src/Data/PartialOrd.hs
+++ src/Data/PartialOrd.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, DeriveDataTypeable,
- DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 
 module Data.PartialOrd (
     PartialOrd,
@@ -28,6 +28,9 @@ embedOrdering Pr.GT = Data.PartialOrd.GT
 class PartialOrd a where
     pCompare :: a -> a -> PartialOrdering
 
+instance {-# OVERLAPPABLE #-} Pr.Ord a => PartialOrd a where
+    pCompare a b = embedOrdering $ Pr.compare a b
+ 
 instance PartialOrd PartialOrdering where
     pCompare Data.PartialOrd.LT Data.PartialOrd.LT = Data.PartialOrd.EQ
     pCompare Data.PartialOrd.LT _ = Data.PartialOrd.LT
@@ -35,5 +38,3 @@ instance PartialOrd PartialOrdering where
     pCompare Data.PartialOrd.GT Data.PartialOrd.GT = Data.PartialOrd.EQ
     pCompare _ _ = IC
 
-instance Pr.Ord a => PartialOrd a where
-    pCompare x y = embedOrdering $ Pr.compare x y
