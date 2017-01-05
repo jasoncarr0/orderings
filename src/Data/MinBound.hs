437e64d2343c51cc7ebb1b8c26c5d6baab023a89
Jason Carr <jac3559@rit.edu>
2017-01-05 16:23:28 -0500

    Added description to MinBound

--- src/Data/MinBound.hs
+++ src/Data/MinBound.hs
@@ -8,14 +8,18 @@ module Data.MinBound (
 import Prelude hiding (Bounded, minBound)
 import qualified Prelude as Pr (Bounded, minBound)
 
+-- | MinBound defines a minimum bounded element to the set
+-- Whenever there is also an instance for some ordering, it should be true
+-- That no element of a is less than minBound :: a
 class MinBound a where 
     minBound :: a
 
 instance Pr.Bounded a => MinBound a where
     minBound = Pr.minBound
 
+
 instance MinBound [a] where
-	minBound = []
+    minBound = []
 
 enumFromMin :: (Enum a, MinBound a) => [a]
 enumFromMin = enumFrom minBound
